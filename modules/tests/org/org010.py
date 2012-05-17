401849e46fe42e9e14e8862be6b7251b8aaa9b40
James O'Neill <hemebond@gmail.com>
2012-05-17 17:04:13 +1200

    Separate test into functions.

--- modules/tests/org/org010.py
+++ modules/tests/org/org010.py
@@ -1,5 +1,6 @@
-# Set up Offices
+# Manage user access levels on organisations
 __all__ = ["org010"]
+
 # Selenium WebDriver
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -11,17 +12,61 @@ from tests import *
 def org010():
     """
     1. Log in as admin
-    2. Give test user org admin rights over default domain (Timor-Leste)
+    2. Give test user org admin rights over Timor-Leste
     3. Give user margarida.martins@redcross.tl some access on Timor-Leste
     4. Log in as test user
     5. Revoke all access for margarida.martins@redcross.tl on Timor-Leste
+    """
+
+    as_admin()
+    logout()
+
+    as_orgadmin()
+    logout()
+
+def as_admin():
+    """
+    Run the tests as an administrator
     """
     config = current.test_config
     browser = config.browser
     driver = browser
 
-    #homepage()
     login(account='admin')
+    make_user_orgadmin()
+    open_organisation_roles()
+    select_user()
+
+    # Set some new access levels
+    driver.find_element_by_id('role_volvol_read').click()
+    driver.find_element_by_id('role_projectproject_data_entry').click()
+    driver.find_element_by_id('role_projectproject_data_entry').submit()
+
+    # @todo: check the values of the matrix
+
+def as_orgadmin():
+    """
+    Run the tests as an org admin
+    """
+    config = current.test_config
+    browser = config.browser
+    driver = browser
+
+    login()
+    open_organisation_roles()
+    select_user()
+
+    # Reset those access levels back to None
+    driver.find_element_by_id('role_volNone').click()
+    driver.find_element_by_id('role_projectNone').click()
+    driver.find_element_by_id('role_projectNone').submit()
+
+    # @todo: check the values of the matrix
+
+def make_user_orgadmin():
+    config = current.test_config
+    browser = config.browser
+    driver = browser
 
     # Manage the system roles for Test User
     driver.find_element_by_link_text("Administration").click()
@@ -35,34 +80,11 @@ def org010():
     driver.find_element_by_xpath("//select[@name='group_id']/option[@value='6']").click()
     driver.find_element_by_xpath("//select[@name='pe_id']/optgroup[2]/option[@value='77']").click()
     driver.find_element_by_xpath("//select[@name='pe_id']/optgroup[2]/option[@value='77']").submit()
-    
-    # Go to the organisation list
-    driver.find_element_by_link_text("Organizations").click()
-    driver.find_element_by_link_text("List Organizations").click()
 
-    # Go to the next page of the list
-    driver.find_element_by_xpath("//a[@id='list_next']").click()
-
-    # Select Timor-Leste from the list
-    driver.find_element_by_xpath("//a[@href='/eden/org/organisation/35/update']").click()
-
-    # Go to the organisations' User Roles tab
-    driver.find_element_by_link_text("User Roles").click()
-
-    # Select a user from the drop-down list
-    driver.find_element_by_xpath("//select[@name='user']/option[@value='4']").click()
-    driver.find_element_by_xpath("//select[@name='user']/option[@value='4']").submit()
-    
-    # Set some new access levels
-    driver.find_element_by_id('role_volvol_read').click()
-    driver.find_element_by_id('role_projectproject_data_entry').click()
-    driver.find_element_by_id('role_projectproject_data_entry').submit()
-
-    # Log out the admin user
-    logout()
-
-    # Log in as test user
-    login()
+def open_organisation_roles():
+    config = current.test_config
+    browser = config.browser
+    driver = browser
 
     # Go to the organisation list
     driver.find_element_by_link_text("Organizations").click()
@@ -72,19 +94,16 @@ def org010():
     driver.find_element_by_xpath("//a[@id='list_next']").click()
 
     # Select Timor-Leste from the list
-    driver.find_element_by_xpath("//a[@href='/eden/org/organisation/35']").click()
+    driver.find_element_by_xpath("//a[contains(@href, '/organisation/35')]").click()
 
     # Go to the organisations' User Roles tab
     driver.find_element_by_link_text("User Roles").click()
 
+def select_user():
+    config = current.test_config
+    browser = config.browser
+    driver = browser
+
     # Select a user from the drop-down list
     driver.find_element_by_xpath("//select[@name='user']/option[@value='4']").click()
     driver.find_element_by_xpath("//select[@name='user']/option[@value='4']").submit()
-
-    # Reset those access levels back to None
-    driver.find_element_by_id('role_volNone').click()
-    driver.find_element_by_id('role_projectNone').click()
-    driver.find_element_by_id('role_projectNone').submit()
-
-    # Logout
-    logout()
